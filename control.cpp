d8bb46268835239b75f216f034c9d9ecb2bd878f
possibilia <possibilia@hotmail.com>
2022-02-09 07:24:49 +0000

    fixed pointer issues

--- control.cpp
+++ control.cpp
@@ -1,17 +1,17 @@
 #include "control.h"
 
 void Control::forward(float speed) {
-	alphabot.setLeftWheelSpeed(speed);
-	alphabot.setRightWheelSpeed(speed);
+	alphabot->setLeftWheelSpeed(speed);
+	alphabot->setRightWheelSpeed(speed);
 }
 
 void Control::stop() {
-	alphabot.setLeftWheelSpeed(0);
-	alphabot.setRightWheelSpeed(0);
+	alphabot->setLeftWheelSpeed(0);
+	alphabot->setRightWheelSpeed(0);
 }
 
 void Control::turn(float speed) {
-	alphabot.stop();
-	alphabot.setLeftWheelSpeed(speed);
-	alphabot.setRightWheelSpeed(-speed);
+	alphabot->stop();
+	alphabot->setLeftWheelSpeed(speed);
+	alphabot->setRightWheelSpeed(-speed);
 }
