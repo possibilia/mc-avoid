98d535ee0ad76ff5cc4bd77e09ef823dc0aa77ad
possibilia <possibilia@hotmail.com>
2022-03-02 11:18:30 +0000

    amended test program

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
 #include "alphabot.h"
 #include <thread>
 
-void north(AlphaBot* alphabot, float speed);
+void forward(AlphaBot* alphabot, float speed);
 
-void south(AlphaBot* alphabot, float speed);
+void turn(AlphaBot* alphabot, float speed);
 
 int main(int, char **) {
 	AlphaBot alphabot;
 	alphabot.start();
 
 	// start test
-	north(&alphabot, 0.2);
+	forward(&alphabot, 0.2);
 	std::this_thread::sleep_for(std::chrono::seconds(5));
 
-	south(&alphabot, 0.2);
+	turn(&alphabot, 0.2);
 	std::this_thread::sleep_for(std::chrono::seconds(5));
 
+	forward(&alphabot, 0.2);
+	std::this_thread::sleep_for(std::chrono::seconds(5));
+
+	// stop thread
 	alphabot.stop();
 
 	// exit 
 	return 0;
 }
 
-void north(AlphaBot* alphabot, float speed) {
+void forward(AlphaBot* alphabot, float speed) {
 	alphabot->setLeftWheelSpeed(speed);
 	alphabot->setRightWheelSpeed(speed);
 }
 
-void south(AlphaBot* alphabot, float speed) {
-	alphabot->setLeftWheelSpeed(-speed);
+void turn(AlphaBot* alphabot, float speed) {
+	alphabot->setLeftWheelSpeed(0.0);
+	alphabot->setRightWheelSpeed(0.0);
+	alphabot->setLeftWheelSpeed(speed);
 	alphabot->setRightWheelSpeed(-speed);
 }
