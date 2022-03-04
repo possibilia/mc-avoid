3809f619abd3583905f732994381de7f9891402f
possibilia <possibilia@hotmail.com>
2022-03-04 15:48:07 +0000

    testing

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include "alphabot.h"
+#include "a1lidarrpi.h"
+#include <thread>
+#include <string>
+#include <iostream>
+
+class ControlCallback : public AlphaBot::StepCallback {
+public:
+	virtual void step(AlphaBot &alphabot) {
+		unsigned f = 0;
+		unsigned t = 0;
+		unsigned s = 0;
+
+		if ((action == 1) & (t == 0)) {
+			turn(&alphabot, 0.2);
+			f = 0;
+			t = 1;
+			s = 0;
+		} else if ((action == 0) & (f == 0)) {
+			forward(&alphabot, 0.5);
+			f = 1;
+			t = 0;
+			s = 0;
+		} else if ((action == 2) & (s == 0)) {
+			forward(&alphabot, 0.3);
+			f = 0;
+			t = 0;
+			s = 1;
+		}
+	}
+
+	void setAction(unsigned action) {
+		this->action = action;
+	}
+
+private:
+	unsigned action = 0;
+
+	void forward(AlphaBot* alphabot, float speed) {
+		alphabot->setLeftWheelSpeed(speed);
+		alphabot->setRightWheelSpeed(speed);
+	}
+
+	void turn(AlphaBot* alphabot, float speed) {
+		alphabot->setLeftWheelSpeed(0.0);
+		alphabot->setRightWheelSpeed(0.0);
+		alphabot->setLeftWheelSpeed(speed);
+		alphabot->setRightWheelSpeed(-speed);
+	}
+};
+
+class DataInterface : public A1Lidar::DataInterface {
+public:
+	void newScanAvail(float, A1LidarData (&data)[A1Lidar::nDistance]) {
+		if (action == 1) {
+			action = 0;
+		}
+
+		for(A1LidarData &data: data) {
+			if ((data.valid) & (data.r < 0.2) & (data.r >= 0.0) & 
+				(data.phi < 2.0) & (data.phi > -2.0)) {
+				action = 1;
+			} else if ((data.valid) & (data.r >= 0.2) & (data.r < 0.4) & 
+				(data.phi < 2.0) & (data.phi > -2.0)) {
+				action = 2;
+			}
+		}
+	}
+
+	unsigned getAction() {	
+		return action;
+	}
+
+private:
+	unsigned action = 0;
+
+	float * filter(A1LidarData* data) {
+		// filter the data
+	}
+
+};
+
+int main(int, char **) { 
+	DataInterface data;
+	ControlCallback control;
+
+	AlphaBot alphabot;
+	alphabot.registerStepCallback(&control);
+	alphabot.start();
+
+	A1Lidar lidar;
+	lidar.registerInterface(&data);
+	lidar.start();
+
+	while(true) {
+		unsigned action;
+		action = data.getAction();
+		control.setAction(action);
+	}
+
+	alphabot.stop();
+	lidar.stop();
+
+	return 0;
+}
