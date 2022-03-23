fb2d6dc6425be3a279931eec3c243d995d8f83ac
possibilia <possibilia@hotmail.com>
2022-03-23 18:38:41 +0000

    libcurl test

--- curlTest.cpp
+++ curlTest.cpp
@@ -7,30 +7,32 @@ size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
 }
 
 int main(int argc, char** argv) {
-    auto curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.0.111:5000/query?t=1&x=3.4&y=2.3&r=4.5&phi=5.3&sig=0.5");
-        curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-        curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
-        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
-        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
-        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-        
-        std::string response_string;
-        std::string header_string;
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
-        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
-        
-        char* url;
-        long response_code;
-        double elapsed;
-        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
-        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
-        
-        curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-        curl = NULL;
+    while(true) {
+        auto curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.0.111:5000/query?t=1&x=3.4&y=2.3&r=4.5&phi=5.3&sig=0.5");
+            curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+            curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
+            curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
+            curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
+            curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+            
+            std::string response_string;
+            std::string header_string;
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+            curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
+            
+            char* url;
+            long response_code;
+            double elapsed;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+            curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
+            curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
+            
+            curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+            curl = NULL;
+        }
     }
 }
