c37ac7e6e62909d13feedb8833a8b00ff0ae436a
possibilia <possibilia@hotmail.com>
2022-03-23 17:35:46 +0000

    refactor

--- test.cpp
+++ test.cpp
@@ -57,49 +57,7 @@ public:
 				(data.phi < 0.0) & (data.phi > -1.0)) {
 				action = 2;
 			}
-
-			// initialise curl
-			auto curl = curl_easy_init();
-
-			if (curl) {
-				// get timestamp using unix epoch (secs)
-				auto now = std::chrono::system_clock::now();
-				unsigned t = std::chrono::duration_cast<std::chrono::seconds>(
-							 now.time_since_epoch()).count();
-				
-				// build the query string
-				std::string query = "http://192.168.0.111:5000/query?t=" + std::to_string(t) +
-				                    "&x=" + std::to_string(data.x) +
-				                    "&y=" + std::to_string(data.y) +
-				                    "&r=" + std::to_string(data.r) +
-				                    "&phi=" + std::to_string(data.phi) +
-				                    "&sig=" + std::to_string(data.signal_strength);
-
-				// send data to server
-				curl_easy_setopt(curl, CURLOPT_URL, query);
-				curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
-		        curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
-		        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
-		        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
-		        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-
-		        std::string response_string;
-		        std::string header_string;
-		        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, this->writeFunction);
-		        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
-		        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
-
-		        char* url;
-		        long response_code;
-		        double elapsed;
-		        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
-		        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
-		        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
-		        
-		        curl_easy_perform(curl);
-		        curl_easy_cleanup(curl);
-		        curl = NULL;
-			}
+			dumpData(&data);
 		}
 	}
 
@@ -113,6 +71,51 @@ private:
 	static size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
 	    data->append((char*) ptr, size * nmemb);
 	    return size * nmemb;
+	}
+
+	void dumpData(A1LidarData* data) {
+		// initialise curl
+		auto curl = curl_easy_init();
+
+		if (curl) {
+			// get timestamp using unix epoch (secs)
+			auto now = std::chrono::system_clock::now();
+			unsigned t = std::chrono::duration_cast<std::chrono::seconds>(
+						 now.time_since_epoch()).count();
+			
+			// build the query string
+			std::string query = "http://192.168.0.111:5000/query?t=" + std::to_string(t) +
+			                    "&x=" + std::to_string(data.x) +
+			                    "&y=" + std::to_string(data.y) +
+			                    "&r=" + std::to_string(data.r) +
+			                    "&phi=" + std::to_string(data.phi) +
+			                    "&sig=" + std::to_string(data.signal_strength);
+
+			// send data to server
+			curl_easy_setopt(curl, CURLOPT_URL, query);
+			curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
+	        curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
+	        curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
+	        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
+	        curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
+
+	        std::string response_string;
+	        std::string header_string;
+	        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, this->writeFunction);
+	        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
+	        curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
+
+	        char* url;
+	        long response_code;
+	        double elapsed;
+	        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+	        curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
+	        curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &url);
+	        
+	        curl_easy_perform(curl);
+	        curl_easy_cleanup(curl);
+	        curl = NULL;
+		}
 	}
 
 };
