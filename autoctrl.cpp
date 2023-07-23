81d94a31b4d946494740ad729097794bd31e4134
Chris Chandler <possibilia@hotmail.com>
2023-07-23 19:04:54 +0100

    control structure

--- autoctrl.cpp
+++ autoctrl.cpp
@@ -4,81 +4,160 @@
 #include <signal.h>
 #include <vector>
 #include <typeinfo>
-// #include<chrono>
+#include <iostream>
+#include <stdio.h>
+#include <termios.h>
+#include <list>
+//#include <string>
 
 using namespace std;
-using namespace std::chrono;
+//using namespace std::chrono;
 
-const float safeDistance = 0.2; 
-const float maxDetectRange = 1.0; 
+const float safeDistance = 0.15; 
+const float maxDetectRange = 2.0; 
 
 const float rThresh = 0.05;
 const float phiThresh = 0.1;
+const float xThresh = 0.01;
 
-Logger logger;
+const float motorSpeed = 0.3;
+const float tolerance = 0.0250010341405868;
 
 bool running = true;
 
+const char* experiment = "2";
+
+//Logger logger;
+
 // terminate sig handler
 void sig_handler(int signo) {
-	if (signo == SIGINT) {
-		running = false;
-	}
+    if (signo == SIGINT) {
+            running = false;
+    }
 }
 
+int getch(void) {
+    int ch;
+
+    struct termios oldt;
+    struct termios newt;
+  
+    tcgetattr(STDIN_FILENO, &oldt);
+
+    newt = oldt; 
+    newt.c_lflag &= ~(ICANON | ECHO); 
+  
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+    ch = getchar(); 
+  
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+  
+    return ch;
+}
+
+
 // every 200 ms
+// 8192 data points 
+// (invalid: corrupted/outside detection zone)
+// scan event -> actor event handler
 class DataInterface : public A1Lidar::DataInterface {
 public:
-	Actor& actor;
+    Actor& actor;
 
-	// wtf does this construction mean?
-	DataInterface(Actor& _actor) : actor(_actor) {};
+    DataInterface(Actor& _actor) : actor(_actor) {};
 
-	void newScanAvail(float, A1LidarData (&data)[A1Lidar::nDistance]) {
-		int nData = 0;
+public:
+    void newScanAvail(float rpm, A1LidarData (&data)[A1Lidar::nDistance]) {
+        int nData = 0;
 
-		vector<ObjectIdentifier> objects;
-		for(A1LidarData &data: data) {
-			// do we really want invalid data?
-			// just create objects on the heap
-			// which do not have any point info
-			ObjectIdentifier object;
-			if (data.valid && data.r > safeDistance 
-				&& data.r < maxDetectRange) {
-				object.setLocation(data.x, data.y);
-				nData++;
-			}
-			objects.push_back(object);
-		}
-		actor.eventNewRelativeCoordinates(objects);
-		logger.printf("nData = %d", nData);
-	}
+        vector<ObjectIdentifier> objects;
+        ObjectIdentifier object; // invalid by default
+
+        for(A1LidarData &data: data) {
+            if (data.valid && data.r > safeDistance 
+                && data.r < maxDetectRange) {
+                object.setObject(data.x, data.y);
+                nData++;
+            } 
+            objects.push_back(object);
+        }
+        actor.eventNewRelativeCoordinates(rpm/60.0f, objects);
+        //logger.printf("RPM = %f, Samplerate = %f\n", rpm, rpm/60.0f);
+    }
+};
+
+// closes the loop
+// callback to robot actuators from task event
+// initiated by a scan event from the lidar
+class RoboTakeAction : public TakeAction {
+public:
+    RoboTakeAction(AlphaBot& _alphabot) : alphabot(_alphabot) {}
+
+    AlphaBot& alphabot;
+    virtual void motorAction(float l, float r) {
+        //if (AIon) {
+            alphabot.setLeftWheelSpeed(l);
+            alphabot.setRightWheelSpeed(r);
+        //}
+    }
 };
 
 int main(int, char **) { 
-	signal(SIGINT, sig_handler);
-	Actor actor;
+    signal(SIGINT, sig_handler);
 
-	DataInterface data(actor);
-	A1Lidar lidar;
+    Actor actor;
 
-	lidar.registerInterface(&data);
-	lidar.start();
+    DataInterface data(actor);
+    A1Lidar lidar;
 
-	logger.startLogging("../test/autoctrl.txt", true);
-	// TestLogger test;
+    lidar.registerInterface(&data);
+    lidar.start();
 
-	//AlphaBot alphabot;
-	//alphabot.registerStepCallback(&control);
-	//alphabot.start();
+    AlphaBot alphabot;
+    RoboTakeAction takeAction(alphabot);
 
-	//alphabot.setLeftWheelSpeed(-0.5);
-	//alphabot.setRightWheelSpeed(0.5);
+    shared_ptr<AbstractTask> targetTask = make_shared<StraightTask>();
+    targetTask->registerTakeAction(&takeAction);
+    actor.setTargetTask(targetTask);
 
-	while(running) {
-		// do nothing
-		// test.test("hello\n");
-	}
+    alphabot.start();
 
-	return 0;
+    logger.startLogging("../test/manualctrl.txt", true);
+    
+    while(running) {
+        int ch = getch();
+        switch (ch) {
+            case ' ':
+                alphabot.setLeftWheelSpeed(0);
+                alphabot.setRightWheelSpeed(0);
+                logger.printf("STOP!\n");
+                break;
+            case 'a':
+                alphabot.setLeftWheelSpeed(-motorSpeed);
+                alphabot.setRightWheelSpeed(motorSpeed);
+                logger.printf("Initiating turning left...\n");
+                break;
+            case 'd':
+                alphabot.setLeftWheelSpeed(motorSpeed);
+                alphabot.setRightWheelSpeed(-motorSpeed);
+                logger.printf("Initiating turning right...\n");
+                break;
+            case 's':
+                alphabot.setLeftWheelSpeed(-motorSpeed);
+                alphabot.setRightWheelSpeed(-motorSpeed);
+                logger.printf("Initiating move backwards...\n");
+                break;
+            case 'w':
+                alphabot.setLeftWheelSpeed(motorSpeed);
+                alphabot.setRightWheelSpeed(motorSpeed);
+                logger.printf("Initiating moving forward...\n");
+                break;
+            default:
+                break;
+        }
+    }
+
+    lidar.stop();
+    alphabot.stop();
+    return 0;
 }
--- lib/lib.h
+++ lib/lib.h
@@ -4,67 +4,150 @@
 #include "logger.h"
 #include <vector>
 #include <cassert>
+#include <math.h>
+#include <memory>
 
 using namespace std;
 
 extern int nEvents;
 
-class Point {
-public:
+////////////////////////////////// Object handling ////////////////////////////
+
+struct Point {
 	float x = 0.0;
 	float y = 0.0;
-	
-	Point() {}
-
-	Point(float x, float y) {
-		set(x, y);
-	}
-
-	void set(float x, float y) {
-		this->x = x;
-		this->y = y;
-	}
 };
 
 class ObjectIdentifier {
+
 public:
+	enum class ObjectType {invalid, obstacle, target};
 
 	ObjectIdentifier() {}
 
-	ObjectIdentifier(float x, float y) {
-		setLocation(x, y);
+	ObjectIdentifier(float x, float y, ObjectType t = ObjectType::obstacle) {
+		setObject(x, y, t);
 	}
 
-	void setLocation(float x, float y) {
-		Point l(x, y);
-		location = l;
+	void setObject(float x, float y, ObjectType t = ObjectType::obstacle) {
+		location.x = x;
+		location.y = y; 
+		label = t;
 	}
 
 	Point getLocation() const {
+		assert(isValid());
 		return location;
+	}
+
+	float getDistance(ObjectIdentifier const &loc2) const {
+		assert( isValid() && loc2.isValid());
+		float a = pow(location.x - loc2.getLocation().x, 2); 
+		float b = pow(location.y - loc2.getLocation().y, 2);
+		return sqrt(a + b);
+	}
+
+	float getAngle() const {
+		assert(isValid());
+		return -atan2(location.y, location.x);
+	}
+
+	ObjectType getLabel() const {
+		return label;
+	}
+
+	void invalidate() {
+		assert(isValid());
+		label = ObjectType::invalid;
+	}
+
+	bool isValid() const {
+		return ObjectType::invalid != label;
 	}
 
 private:
 	Point location;
+	ObjectType label = ObjectType::invalid;
 };
+
+
+////////////////////////////////// Task hierarchy ////////////////////////////
+
+// abstract intterface to control the motors
+struct TakeAction {
+	virtual void motorAction(float speedLeft, float speedRight) = 0;
+};
+
+class AbstractTask {
+public:
+	enum ResultCodes { nothing = 0, 
+	disturbance_gone = 1, failed = 2, new_disturbance = 3 };
+
+	struct TaskResult {
+		ResultCodes result = nothing;
+		ObjectIdentifier newDisturbance;
+		void setDisturbance(ObjectIdentifier d) {
+			newDisturbance = d;
+			result = new_disturbance;
+		}
+	};
+
+	virtual TaskResult robotExecutionStep(float samplingrate, 
+		const vector<ObjectIdentifier>& objects) = 0;
+
+	void registerTakeAction(TakeAction* ta) {
+		takeAction = ta;
+	}
+
+protected:
+	void newMotorAction() {
+		if (nullptr != takeAction) {
+			takeAction->motorAction(motorDriveLeft,motorDriveRight);
+		}
+	}
+
+	TakeAction* takeAction = nullptr;
+	float motorDriveLeft = 0.2;
+	float motorDriveRight = 0.2;
+	float robotDrive2realSpeed = 0.1;
+	ObjectIdentifier disturbance;
+};
+
+struct StraightTask : AbstractTask {
+	virtual TaskResult robotExecutionStep(float samplingrate, 
+		const vector<ObjectIdentifier>& objects);
+};
+
+
+//////////////////////////// Actor event handler //////////////////////////////
+
 
 class Actor {
 public:
-	void eventNewRelativeCoordinates(
+	void eventNewRelativeCoordinates(float samplingrate,
 		const vector<ObjectIdentifier>& objects);
 
+	void setTargetTask(shared_ptr<AbstractTask> t) {
+		targetTask = t;
+	}
+
 private:
+
+	vector<ObjectIdentifier> previousTrackingPoints;
+
+	shared_ptr<AbstractTask> targetTask;
+
 	void saveMap(const vector<ObjectIdentifier>& objects) {
 		char tmp[256];
 		sprintf(tmp,"../test/map%03d.dat",nEvents);
 		fprintf(stderr,"%s\n",tmp);
 		FILE* f = fopen(tmp,"wt");
 		for(unsigned i = 0; i < objects.size();i++) {
-			//if (objects[i].isValid()) {
+			if (objects[i].isValid()) {
 				fprintf(f,"%4.4f %4.4f\n",
 				objects[i].getLocation().x,
 				objects[i].getLocation().y);
-			//}
+			}
 		}
 		fclose(f);
 	}
